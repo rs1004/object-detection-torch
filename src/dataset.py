fd54e16687b9fe99321fd48c4a73ea5378d5827c
rs1004 <r82.y25@gmail.com>
2020-05-26 14:50:07 +0000

    [add] バグ修正、動作確認

--- src/dataset.py
+++ src/dataset.py
@@ -2,6 +2,7 @@ from torch.utils.data import Dataset
 from PIL import Image
 from pathlib import Path
 import xml.etree.ElementTree as ET
+import torch
 
 LABEL_MAP = {
     'aeroplane': 0,
@@ -28,12 +29,14 @@ LABEL_MAP = {
 
 
 class MyDataset(Dataset):
-    def __init__(self, data_dir, data_list_file_name, imsize=448, grid_num=7, bbox_num=2, transform=None):
+    def __init__(self, data_dir, data_list_file_name, imsize=448, grid_num=7, bbox_num=2, l_coord=5., l_noobj=0.5, transform=None):
         self.transform = transform
         self.data_paths = self._get_paths(data_dir, data_list_file_name)
         self.imsize = imsize
         self.grid_num = grid_num
         self.bbox_num = bbox_num
+        self.l_coord = l_coord
+        self.l_noobj = l_noobj
 
     def __len__(self):
         return len(self.data_paths)
@@ -57,13 +60,13 @@ class MyDataset(Dataset):
 
         return [[Path(data_dir) / 'JPEGImages' / f'{i}.jpg', Path(data_dir) / 'Annotations' / f'{i}.xml'] for i in ids[:-1]]
 
-    def _get_label(label_path):
+    def _get_label(self, label_path):
         grid_len = self.imsize // self.grid_num
 
         root = ET.parse(label_path).getroot()
         width, height, _ = [int(s.text) for s in root.find('size')]
-        label = np.zeros((self.grid_num, self.grid_num, 5 * self.bbox_num + self.class_num))
-        mask = label.copy()
+        label = torch.zeros((self.grid_num, self.grid_num, 5 * self.bbox_num + len(LABEL_MAP)))
+        mask = torch.zeros_like(label)
         for obj in root.iter('object'):
             xmin, ymin, xmax, ymax = [int(b.text) for b in obj.find('bndbox')]
             xmin, xmax = self.imsize * xmin / width, self.imsize * xmax / width
@@ -73,17 +76,16 @@ class MyDataset(Dataset):
             lx, ly = xmax - xmin, ymax - ymin
             idx, idy = int(cx // grid_len), int(cy // grid_len)
             for j in range(self.bbox_num):
-                if label[idx, idy, 5*j] == 0.:    
-                    label[idx, idy, 0+5*j:2+5*j] = (np.array([cx, cy]) % grid_len) / grid_len
-                    label[idx, idy, 2+5*j:4+5*j] = np.sqrt((np.array([lx, ly]) / self.imsize))
+                if label[idx, idy, 5*j] == 0.:
+                    label[idx, idy, 0+5*j:2+5*j] = (torch.tensor([cx, cy]) % grid_len) / grid_len
+                    label[idx, idy, 2+5*j:4+5*j] = torch.sqrt((torch.tensor([lx, ly]) / self.imsize))
                     label[idx, idy, 4+5*j] = 1.
-                    mask[idx, idy, 0+5*j:4+5*j] = lmd
+                    mask[idx, idy, 0+5*j:4+5*j] = self.l_coord
                     mask[idx, idy, 4+5*j] = 1
                     break
             label[idx, idy, 10+c] = 1.
             mask[idx, idy, 10:] = 1.
         for j in range(self.bbox_num):
-            mask[:, :, 4+5*j] += (1 - mask[:, :, 4+5*j]) * 0.5
-        
-        return np.array([label, mask])
-    
+            mask[:, :, 4+5*j] += (1 - mask[:, :, 4+5*j]) * self.l_noobj
+
+        return torch.stack([label, mask])
---
+++ src/run.py
@@ -0,0 +1,16 @@
+from dataset import MyDataset
+import torch
+import torchvision.transforms as transforms
+
+
+transform = transforms.Compose([transforms.ToTensor()])
+dataset = MyDataset(data_dir='/work/data/VOCdevkit/VOC2007',
+                    data_list_file_name='trainval.txt',
+                    transform=transform)
+
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=4,
+                                         shuffle=True, num_workers=2)
+
+for images, labels in dataloader:
+    print(labels[0])
+    break
